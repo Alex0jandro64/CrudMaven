4124b8b845225ec2c912f788747c4d6ae2050950
Alex0jandro64 <alejandroalcerreca3@gmail.com>
2023-10-19 10:10:28 +0200

    Initial commit

---
+++ src/main/java/servicios/LibroInterfaz.java
@@ -0,0 +1,28 @@
+package servicios;
+
+import java.util.ArrayList;
+
+import dtos.LibroDto;
+/**
+ * Interfaz que contiene todos los metodos que modifca la lista de objetos LibroDTO
+ */
+public interface LibroInterfaz {
+/**
+ * Metodo que pide los datos y crea un objeto libro para añadirlo a la lista 
+ * @param listaAntigua
+ */
+	public void creaLibro(ArrayList<LibroDto> listaAntigua);
+	
+	/**
+	 * Metodo que pide un id_libro para añadirlo a un objeto que posteriormente se eliminara de la bd
+	 * @param listaAntigua
+	 */
+	public void pideId(ArrayList<LibroDto> listaAntigua);
+	
+	/**
+	 * Metodo que pide que campo quiere modifica y lo devuelve 
+	 * @param listaAntigua
+	 * @return
+	 */
+	public int datosAModificar(ArrayList<LibroDto> listaAntigua);
+}
